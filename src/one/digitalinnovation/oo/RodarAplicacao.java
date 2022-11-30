14b2de37de953212908e96bd5b9c0f4e04ac9944
Bruna Quinto Marsiaj <110314990+Bruna-Quinto-Marsiaj@users.noreply.github.com>
2022-11-30 20:53:10 -0300

    observacoes polimorfismo e sobrescrita

--- src/one/digitalinnovation/oo/RodarAplicacao.java
+++ src/one/digitalinnovation/oo/RodarAplicacao.java
@@ -9,19 +9,20 @@ class RodarAplicacao {
 
         ClasseMae[] classes = new ClasseMae[]{new ClasseFilha1(), new ClasseFilha2(), new ClasseMae()};
 
-        for (ClasseMae classe: classes) {
-            classe.metodo1();
-        }
+        for (ClasseMae classe: classes) {// nas classes filhas os métodos foram sobrescritos e neles colocados a annotation override.
+             classe.metodo1();           //O for ta com "Chamando" o tipo de dado classe mãe, quando passa pela filha acontece polimorfismo, pq ele identifica o método "igual"
+        }                            
+            
 
         System.out.println("");
 
-        for (ClasseMae classe: classes) {
+        for (ClasseMae classe: classes) { //classe filha um não sobrescreveu o método 2, então no lugar dele chama o método 2 da classe mãe
             classe.metodo2();
         }
 
         System.out.println("");
 
-        ClasseFilha2 classeFilha2 = new ClasseFilha2();
+        ClasseFilha2 classeFilha2 = new ClasseFilha2(); //aqui acontece a sobrescrita pura, chama direto o método sobrescrito e não através da classe mãe (polimorfismo)
         classeFilha2.metodo2();
     }
 
